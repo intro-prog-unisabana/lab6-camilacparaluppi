c0e7020e743a399aba4dde098c28e1d511e94201
Camila C <camilacparaluppiiiii@gmail.com>
2026-03-27 11:41:20 -0500

    problema5

--- overall.py
+++ overall.py
@@ -2,17 +2,19 @@ def student_averages(students):
     result = {}
     for student, notas in students.items():
         suma = 0
+        cantidad = 0
         for tarea, nota in notas.items():
             suma = suma + nota
-        promedio = round(suma / len(students)) 
-        result = promedio 
-    return result
+            cantidad = cantidad + 1
+        promedio = round(suma / cantidad)  
+        result[student] = promedio       
+        return result
 def assignment_averages(students):
     result = {}
     for student, notas in students.items():
         for tarea, nota in notas.items():
             if tarea in result:
-                result[tarea] = nota  
+                result[tarea] = result[tarea] + nota   
             else:
                 result[tarea] = nota
     for tarea in result:
