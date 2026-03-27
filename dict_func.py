8f5359441f4bb4af9c890dc460a9f1036465f5d4
Camila C <camilacparaluppiiiii@gmail.com>
2026-03-27 10:52:45 -0500

    problema3

--- dict_func.py
+++ dict_func.py
@@ -3,6 +3,7 @@ def employee_print(employee_info):
     print("Name:", employee_info.get("Name", "N/A"))
     print("Salary:", employee_info.get("Salary", "N/A"))
     print("Role:", employee_info.get("Role", "N/A"))
+    keydiferente = False
     for key in employee_info:
         if key != "Name" and key != "Salary" and key != "Role":
             print(key, ":", employee_info[key])
