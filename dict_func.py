9f3c0900a55300d2aed60232742ef5e9caaea685
Camila C <camilacparaluppiiiii@gmail.com>
2026-03-27 10:30:16 -0500

    problema3

--- dict_func.py
+++ dict_func.py
@@ -1,13 +1,15 @@
 # Write your code here!
 def employee_print(employee_info):
-    employee_info = {"Name": "", "Salary": "", "Role": ""}
+    print(employee_info.get("Name", "N/A"))
+    print(employee_info.get("Salary", "N/A"))
+    print(employee_info.get("Role", "N/A"))
     if "Name" == "" and "Salary" == "" and "Role" == "":
-        print ("No other info!")
+        print("No other info!")
     elif "Name" == "":
-        print ("N/A")
+        print("N/A")
     elif "Salary" == "":
-        print ("N/A")
+        print("N/A")
     elif "Role" == "":
-        print ("N/A")
+        print("N/A")
     elif "keys" in employee_info:
-        print ("N/A")
+        print("N/A")
