41a438574f943cb6a36d69daecaca6eb71f8e5f6
Camila C <camilacparaluppiiiii@gmail.com>
2026-03-27 10:19:08 -0500

    problema3

--- dict_func.py
+++ dict_func.py
@@ -1,1 +1,13 @@
 # Write your code here!
+def employee_print(employee_info):
+    employee_info = {"Name": "", "Salary": "", "Role": ""}
+    if "Name" == "" and "Salary" == "" and "Role" == "":
+        print ("No other info!")
+    elif "Name" == "":
+        print ("N/A")
+    elif "Salary" == "":
+        print ("N/A")
+    elif "Role" == "":
+        print ("N/A")
+    elif "keys" in employee_info:
+        print ("N/A")
