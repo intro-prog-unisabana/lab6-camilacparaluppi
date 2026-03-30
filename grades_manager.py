ca77ee04c7155ba72cec5f26a655c38de736ee29
Camila C <camilacparaluppiiiii@gmail.com>
2026-03-30 12:00:57 -0500

    problema6II

--- grades_manager.py
+++ grades_manager.py
@@ -1,2 +1,16 @@
 def initialize_dict(student_name, subject_grades):
     return {student_name: subject_grades}
+def add_student(student_grades={}):
+    name = input("Enter student name: ").title()
+    subjects = {}
+    while True:
+        mensajeentrada = input("Enter subject and grade (or 'exit' to finish): ").lower()
+        if mensajeentrada == "exit":
+            break
+        materia_nota = mensajeentrada.split(",")
+        subject = materia_nota[0].title()
+        grade = float(materia_nota[1])
+        subjects[subject] = grade
+    student_grades[name] = subjects
+    print(f"Student {name} successfully added to the grades management system.")
+    return student_grades
